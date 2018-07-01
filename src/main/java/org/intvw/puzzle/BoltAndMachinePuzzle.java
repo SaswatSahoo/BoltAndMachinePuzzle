ef936d13252ddf95fff5fdda5c6a1e791737f0c0
SaswatSahoo <40638549+SaswatSahoo@users.noreply.github.com>
2018-07-01 15:51:09 +0530

    Add files via upload

---
+++ src/main/java/org/intvw/puzzle/BoltAndMachinePuzzle.java
@@ -0,0 +1,63 @@
+package org.intvw.puzzle;
+
+public class BoltAndMachinePuzzle
+{
+	public static void main(String[] args)
+	{
+		char mach[] = {'@', '#', '$', '%', '^', '&'};
+		char bolts[] = {'$', '%', '&', '^', '@', '#'};
+		
+		matchPairs(mach, bolts, 0, 5);
+
+		System.out.println("Matched mach and bolts are : ");
+		
+		printArray(mach);
+		printArray(bolts);
+	}
+
+	private static void printArray(char[] arr) {
+		for (char ch : arr){
+			System.out.print(ch + " ");
+		}
+		System.out.print("n");
+	}
+
+	private static void matchPairs(char[] nuts, char[] bolts, int low,
+															int high)
+	{
+		if (low < high)
+		{
+			int pivot = partition(nuts, low, high, bolts[high]);
+
+			partition(bolts, low, high, nuts[pivot]);
+
+			matchPairs(nuts, bolts, low, pivot-1);
+			matchPairs(nuts, bolts, pivot+1, high);
+		}
+	}
+
+	private static int partition(char[] arr, int low, int high, char pivot)
+	{
+		int i = low;
+		char temp1, temp2;
+		for (int j = low; j < high; j++)
+		{
+			if (arr[j] < pivot){
+				temp1 = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp1;
+				i++;
+			} else if(arr[j] == pivot){
+				temp1 = arr[j];
+				arr[j] = arr[high];
+				arr[high] = temp1;
+				j--;
+			}
+		}
+		temp2 = arr[i];
+		arr[i] = arr[high];
+		arr[high] = temp2;
+
+		return i;
+	}
+}
